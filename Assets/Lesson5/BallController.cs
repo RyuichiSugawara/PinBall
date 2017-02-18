97b51d77b2911714f92e1cc6c1e28cef6d45eef5
菅原龍一 <ryuichi_fjb@yahoo.co.jp>
2017-02-18 15:26:18 +0900

    ピンボールプロジェクトの課題
    課題提出スマートフォン向けは次のレッスン後に

---
+++ Assets/Lesson5/BallController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+	//得点の変数づくり
+	public Text scoreText ;
+	private int score = 0;
+	private Transform playerTrans;
+
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+		score   = 0;
+		SetScore();   //初期スコアを代入して表示
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+			
+		// ボールがあたるものによって点数を変える
+		SetScore();
+
+	}
+	void OnCollisionEnter (Collision score_tag) {
+		if (score_tag.gameObject.tag == "SmallStarTag") {
+			score += 0; 
+			Debug.Log ("zero");
+		} else if (score_tag.gameObject.tag == "LargeStarTag") {
+			score += 20;
+			Debug.Log ("+20");
+		}else if(score_tag.gameObject.tag == "SmallCloudTag" || score_tag.gameObject.tag == "LargeCloudTag") {
+			score += 10;
+			Debug.Log ("+10");
+
+		}
+		SetScore();
+
+	}
+	void SetScore()
+	{
+		scoreText.text = "得点" + score.ToString ();
+	}
+}
---
+++ Assets/Lesson5/ScoreController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreController : MonoBehaviour {
+	//得点の変数づくり
+	public Text scoreText ;
+	private int score = 0;
+	private Transform playerTrans;
+	// Use this for initialization
+	void Start () {
+		scoreText.text = "得点:0";//初期値
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text ="得点" + score ;
+	}
+
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision other) {
+
+	}
+}
